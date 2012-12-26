c11dfc3a071343b3a2c1bd8b773c364023e5d168
Channy <channy@gmail.com>
2012-12-26 13:40:23 -0500

    Add initial files

---
+++ ko/includes/footer.php
@@ -0,0 +1,38 @@
+ </div><!-- close #wrapper -->
+ <footer id="colophon">
+      <div class="row">
+
+          <div class="footer-logo">
+              <a href="http://www.mozilla.or.kr/"><img src="http://www.mozilla.org/media/img/sandstone/footer-mozilla.png" alt="mozilla"></a>
+          </div>
+
+          <div class="footer-license">
+              <p>
+                ©1998–2012 일부 콘텐츠는mozilla.org 공헌자에 의해 만들어졌으며, 이 콘텐츠는 <a href="/foundation/licensing/website-content.html">Creative Commons license</a>에서 사용 가능합니다.</p>
+              <p>
+                <a href="http://www.mozilla.or.kr/ko/contribute/page/">공헌자 홈페이지</a>
+              </p>
+          </div>
+          <ul class="footer-nav">
+            <li><a href="/ko/about/contact.html#map-mountain_view">연락하기</a></li>
+            <li><a href="/ko/privacy/">개인 정보 정책(영문)</a></li>
+            <li><a href="/ko/about/legal.html">법적 고지(영문)</a></li>
+            <li><a href="/ko/legal/fraud-report/index.html">상표권 침해 신고(영문)</a></li>
+          </ul>
+
+                    <ul class="footer-nav">
+            <li><a href="http://twitter.com/mozillakr">Twitter</a></li>
+            <li><a href="http://facebook.com/mozillakr">Facebook</a></li>
+            <li><a href="https://affiliates.mozilla.org/">Firefox 제휴</a></li>
+          </ul>
+          
+      </div>
+      </footer>
+    
+    <script src="/ko/tabzilla/tabzilla.js"></script>
+    </div><!-- close #outer-wrapper -->
+
+      <script src="http://www.mozilla.org/media/js/mozorg-resp-min.js?build=56e9bb3"></script>
+      <?=$extra_js?>
+</body>
+</html>
---
+++ sample.php
@@ -0,0 +1,12 @@
+<? 
+	$Htitle="Mozilla 제품";
+	$Hid="products-page";
+	$extra_css='<link rel="stylesheet" media="screen,projection,tv" href="http://www.mozilla.org/media/css/products-min.css?build=56e9bb3" />';
+
+	include_once "$_SERVER['DOCUMENT_ROOT']/ko/includes/header.php";
+?>
+
+<?
+	$extra_js="";
+	include_once "$_SERVER['DOCUMENT_ROOT']/ko/includes/footer.php";
+?>
