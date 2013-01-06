ad67f0b3445bda25e4c4bc69d56fe9828e0da2bc
Channy <channy@gmail.com>
2013-01-06 19:29:44 +0900

    #5 Reorganize common modules fix for sample.php

--- sample.php
+++ sample.php
@@ -1,7 +1,10 @@
 <?
+ 	require $_SERVER['DOCUMENT_ROOT']."/ko/includes/common.inc";
+	
 	$Htitle="Mozilla 제품";
 	$Hid="products-page";
-	$extra_css='<link rel="stylesheet" media="screen,projection,tv" href="http://www.mozilla.org/media/css/products-min.css?build=56e9bb3" />';
+	
+	$extra_css='<link rel="stylesheet" media="screen,projection,tv" href="http://www.mozilla.org/media/css/products-min.css?build='.$build_code.'" />';
 
 	include $_SERVER['DOCUMENT_ROOT']."/ko/includes/header.php";
 ?>
