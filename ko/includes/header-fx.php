b8b221642e13e26ee366b999e817a30e4e90e88a
Seong-wook Gong <arbi316@gmail.com>
2013-01-09 05:32:12 +0900

    changed some paths and strings

--- ko/includes/header-fx.php
+++ ko/includes/header-fx.php
@@ -24,36 +24,36 @@
         <nav id="nav-main" role="navigation">
   <span class="toggle" role="button" aria-controls="nav-main-menu" tabindex="0">주요 메뉴</span>
   <ul id="nav-main-menu" class="has-submenus">
-    <li class="first" id="nav-main-features"><a href="/ko/firefox/#desktop" tabindex="0" aria-owns="nav-main-features-submenu" aria-haspopup="true">데스크톱</a>
+    <li class="first" id="nav-main-features"><a href="/ko/firefox/fx/#desktop" tabindex="0" aria-owns="nav-main-features-submenu" aria-haspopup="true">데스크톱</a>
       <ul aria-expanded="false" id="nav-main-features-submenu" class="submenu">
         <li class="first"><a href="/ko/firefox/features/" tabindex="-1">주요 기능</a></li>
         <li><a href="/ko/firefox/customize/" tabindex="-1">맞춤 기능</a></li>
-        <li><a href="/ko/firefox/performance/" tabindex="-1">성능 향상</a></li>
+        <li><a href="/ko/firefox/performance/" tabindex="-1">빠른 성능</a></li>
         <li><a href="/ko/firefox/technology/" tabindex="-1">기술 혁신</a></li>
         <li class="last"><a href="/ko/firefox/security/" tabindex="-1">정보 보호 및 보안</a></li>
       </ul>
     </li>
-    <li id="nav-main-mobile"><a href="/ko/firefox/" tabindex="0" aria-owns="nav-main-mobile-submenu" aria-haspopup="true">모바일</a>
+    <li id="nav-main-mobile"><a href="/ko/firefox/fx/" tabindex="0" aria-owns="nav-main-mobile-submenu" aria-haspopup="true">모바일</a>
       <ul aria-expanded="false" id="nav-main-mobile-submenu" class="submenu">
         <li class="first"><a href="/ko/firefox/mobile/features/" tabindex="-1">주요 기능</a></li>
         <li><a href="https://addons.mozilla.org/ko/mobile/?browse=featured" tabindex="-1">맞춤 기능</a></li>
         <li class="last"><a href="/ko/firefox/mobile/faq/" tabindex="-1">FAQ</a></li>
       </ul>
     </li>
-    <li id="nav-main-releases" class=""><a href="/ko/firefox/channel/" tabindex="0" aria-owns="nav-main-releases-submenu" aria-haspopup="true">제품 채널</a>
+    <li id="nav-main-releases" class=""><a href="/ko/firefox/channel/" tabindex="0" aria-owns="nav-main-releases-submenu" aria-haspopup="true">출시 현황</a>
       <ul aria-expanded="false" id="nav-main-releases-submenu" class="submenu">
-        <li class="first"><a href="/ko/firefox/channel/" tabindex="-1">채널 소개</a></li>
+        <li class="first"><a href="/ko/firefox/channel/" tabindex="-1">전체 보기</a></li>
         <li><a href="/ko/firefox/aurora/" tabindex="-1">Firefox Aurora</a></li>
-        <li><a href="/ko/firefox/beta/" tabindex="-1">Firefox Beta</a></li>
+        <li><a href="/beta/" tabindex="-1">Firefox Beta</a></li>
         <li><a href="/ko/firefox/" tabindex="-1">Firefox</a></li>
-        <li class="last"><a href="/ko/firefox/organizations/" tabindex="-1">Firefox 기업용</a></li>
+        <li class="last"><a href="/ko/firefox/organizations/" tabindex="-1">기업용 Firefox</a></li>
       </ul>
     </li>
     <li id="nav-main-addons"><a href="https://addons.mozilla.org/" tabindex="0" aria-owns="nav-main-addons-submenu" aria-haspopup="true">부가 기능</a>
       <ul aria-expanded="false" id="nav-main-addons-submenu" class="submenu">
         <li class="first"><a href="https://addons.mozilla.org/firefox/" tabindex="-1">데스크톱용</a></li>
         <li><a href="https://addons.mozilla.org/mobile/" tabindex="-1">모바일용</a></li>
-        <li class="last"><a href="http://www.getpersonas.com/" tabindex="-1">Personas</a></li>
+        <li class="last"><a href="http://www.getpersonas.com/" tabindex="-1">페르소나</a></li>
       </ul>
       </li>
       <li id="nav-main-support"><a href="https://support.mozilla.org/" tabindex="0" aria-owns="nav-main-support-submenu" aria-haspopup="true">도움말</a>
@@ -69,7 +69,7 @@
 		<li><a href="http://www.mozilla.or.kr/community/" tabindex="-1">블로그</a></li>
         <li><a href="/ko/contribute" tabindex="-1">참여하기</a></li>
         <li><a href="http://www.mozilla.or.kr/community/about" tabindex="-1">한국 커뮤니티</a></li>
-        <li class="last"><a href="/ko/about/contact" tabindex="-1">연락처</a></li>
+        <li class="last"><a href="/ko/about/contact.html" tabindex="-1">연락처</a></li>
       </ul>
     </li>
   </ul>
