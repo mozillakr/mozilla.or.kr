33036d509352c2f05d1156e3a9e8809e31f5544b
Channy Yun <channy@gmail.com>
2013-02-25 17:20:41 +0900

    Add Google Analytics code

--- ko/includes/footer.php
+++ ko/includes/footer.php
@@ -34,5 +34,18 @@
 
       <script src="http://www.mozilla.org/media/js/mozorg-resp-min.js?build=<?=$build_code?>"></script>
       <?=$extra_js?>
+      <script type="text/javascript">
+        var _gaq = _gaq || [];
+       _gaq.push(['_setAccount', 'UA-53785-1']);
+       _gaq.push(['_setDomainName', 'mozilla.or.kr']);
+       _gaq.push(['_trackPageview']);
+
+       (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+     </script>
+
 </body>
 </html>
