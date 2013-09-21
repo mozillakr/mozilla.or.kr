3fe4246ddf03e641efd690d98dc10117675631a3
Hyeonseok Shin <hyeonseok@gmail.com>
2013-09-21 09:12:34 +0900

    Resolve #62 - Update /ko/firefox/aurora/

--- lib/common_rules.php
+++ lib/common_rules.php
@@ -1,5 +1,34 @@
 <?php
 $common_rule = array(
+  // resource path
+  array(
+    'source' => 'href="/includes/', 
+    'target' => 'href="//www.mozilla.org/includes/'), 
+  array(
+    'source' => 'href="/tabzilla/', 
+    'target' => 'href="//www.mozilla.org/tabzilla/'), 
+  array(
+    'source' => 'src="/includes/', 
+    'target' => 'src="//www.mozilla.org/includes/'), 
+  array(
+    'source' => 'href="/style/', 
+    'target' => 'href="//www.mozilla.org/style/'), 
+  array(
+    'source' => 'src="/img/', 
+    'target' => 'src="//www.mozilla.org/img/'), 
+  array(
+    'source' => 'src=\\"/img/', 
+    'target' => 'src=\\"//www.mozilla.org/img/'), 
+  array(
+    'source' => 'url(/img/', 
+    'target' => 'url(//www.mozilla.org/img/'), 
+  array(
+    'source' => 'url(\'/img/', 
+    'target' => 'url(\'//www.mozilla.org/img/'), 
+  array(
+    'source' => 'content="/img/', 
+    'target' => 'content="//www.mozilla.org/img/'), 
+
   // link on head
   array(
     'source' => '<link rel="canonical" hreflang="en-US" href="http://www.mozilla.org/en-US/', 
@@ -104,6 +133,33 @@ $common_rule = array(
         <li><a href="/ko/contribute" tabindex="-1">참여하기</a></li>
         <li><a href="http://www.mozilla.or.kr/community/about" tabindex="-1">한국 커뮤니티</a></li>
         <li class="last"><a href="/ko/about/contact.html" tabindex="-1">연락처</a></li>'), 
+
+  // Aurora Navigation
+  array(
+    'source' => '<a href="#nav-main">skip to Navigation</a>', 
+    'target' => '<a href="#nav-main">메뉴 건너뛰기</a>'), 
+  array(
+    'source' => '<a href="#lang_form">switch language</a>', 
+    'target' => '<a href="#lang_form">언어설정</a>'), 
+  array(
+    'source' => '<a href="/en-US/firefox/" title="Back to home page">Mozilla Firefox</a>', 
+    'target' => '<a href="/ko/firefox/" title="홈페이지">Mozilla Firefox</a>'), 
+  array(
+    'source' => '<a href="http://www.mozilla.org/" class="mozilla" id="tabzilla">mozilla</a>', 
+    'target' => '<a href="http://www.mozilla.or.kr/" class="mozilla" id="tabzilla">mozilla</a>'), 
+  array(
+    'source' => '<li id="nav-main-features"  class="first"><a aria-haspopup="true" aria-owns="nav-main-features-submenu" tabindex="0" href="/en-US/firefox/features/">Desktop</a><ul id="nav-main-features-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="/en-US/firefox/features/">Features</a></li><li ><a tabindex="-1" href="/en-US/firefox/customize/">Customize</a></li><li ><a tabindex="-1" href="/en-US/firefox/performance/">Performance</a></li><li ><a tabindex="-1" href="/en-US/firefox/technology/">Technology</a></li><li  class="last"><a tabindex="-1" href="/en-US/firefox/security/">Privacy &amp; Security</a></li></ul></li>
+    <li id="nav-main-mobile" ><a aria-haspopup="true" aria-owns="nav-main-mobile-submenu" tabindex="0" href="/en-US/mobile/">Mobile</a><ul id="nav-main-mobile-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="/en-US/mobile/">Download</a></li><li ><a tabindex="-1" href="/en-US/mobile/features/">Features</a></li><li ><a tabindex="-1" href="https://addons.mozilla.org/en-US/mobile/?browse=featured">Customize</a></li><li  class="last"><a tabindex="-1" href="/en-US/mobile/faq/">FAQ</a></li></ul></li>
+    <li id="nav-main-releases" ><a aria-haspopup="true" aria-owns="nav-main-releases-submenu" tabindex="0" href="/en-US/firefox/channel/">Releases</a><ul id="nav-main-releases-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="/en-US/firefox/channel/">Overview</a></li><li ><a tabindex="-1" href="/en-US/firefox/aurora/">Firefox Aurora</a></li><li ><a tabindex="-1" href="/beta/">Firefox Beta</a></li><li ><a tabindex="-1" href="/en-US/firefox/">Firefox</a></li><li  class="last"><a tabindex="-1" href="/en-US/firefox/organizations/">Firefox for Organizations</a></li></ul></li>
+    <li id="nav-main-addons" ><a aria-haspopup="true" aria-owns="nav-main-addons-submenu" tabindex="0" href="https://addons.mozilla.org/">Add-ons</a><ul id="nav-main-addons-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="https://addons.mozilla.org/firefox/">Desktop Add-ons</a></li><li ><a tabindex="-1" href="https://addons.mozilla.org/mobile/">Mobile Add-ons</a></li><li  class="last"><a tabindex="-1" href="http://www.getpersonas.com/">Personas</a></li></ul></li>
+    <li id="nav-main-support" ><a aria-haspopup="true" aria-owns="nav-main-support-submenu" tabindex="0" href="http://support.mozilla.org/">Support</a><ul id="nav-main-support-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="http://support.mozilla.org/en-US/kb/">Desktop Support</a></li><li  class="last"><a tabindex="-1" href="http://support.mozilla.org/mobile">Mobile Support</a></li></ul></li>
+    <li id="nav-main-about"  class="last"><a aria-haspopup="true" aria-owns="nav-main-about-submenu" tabindex="0" href="/en-US/firefox/about/">About</a><ul id="nav-main-about-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="http://blog.mozilla.com/">Blog</a></li><li ><a tabindex="-1" href="/en-US/firefox/about/">About Firefox</a></li><li ><a tabindex="-1" href="http://www.mozilla.org/join">Join Mozilla</a></li><li ><a tabindex="-1" href="/en-US/about/participate/">Participate</a></li><li ><a tabindex="-1" href="/en-US/press/">Press Center</a></li><li ><a tabindex="-1" href="/en-US/firefox/brand/">Brand Toolkit</a></li><li ><a tabindex="-1" href="/en-US/about/careers.html">Careers</a></li><li ><a tabindex="-1" href="/en-US/about/partnerships.html">Partnerships</a></li><li  class="last"><a tabindex="-1" href="/en-US/about/contact.html">Contact Us</a></li></ul></li>', 
+    'target' => '<li id="nav-main-features"  class="first"><a aria-haspopup="true" aria-owns="nav-main-features-submenu" tabindex="0" href="/ko/firefox/fx#desktop">데스크탑</a><ul id="nav-main-features-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="/ko/firefox/features/">주요 기능</a></li><li ><a tabindex="-1" href="/ko/firefox/customize/">부가 기능</a></li><li ><a tabindex="-1" href="/ko/firefox/performance/">성능 향상</a></li><li ><a tabindex="-1" href="/ko/firefox/technology/">웹 기술</a></li><li  class="last"><a tabindex="-1" href="/ko/firefox/security/">보안 및 정보 보호</a></li></ul></li>
+    <li id="nav-main-mobile" ><a aria-haspopup="true" aria-owns="nav-main-mobile-submenu" tabindex="0" href="/ko/firefox/fx/">모바일</a><ul id="nav-main-mobile-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="/ko/mobile/">다운로드</a></li><li ><a tabindex="-1" href="/ko/mobile/features/">주요 기능</a></li><li ><a tabindex="-1" href="https://addons.mozilla.org/ko/mobile/?browse=featured">부가 기능</a></li><li  class="last"><a tabindex="-1" href="/ko/mobile/faq/">FAQ &shy;</a></li></ul></li>
+    <li id="nav-main-releases" ><a aria-haspopup="true" aria-owns="nav-main-releases-submenu" tabindex="0" href="/ko/firefox/channel/">다운로드</a><ul id="nav-main-releases-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="/ko/firefox/channel/">채널별</a></li><li ><a tabindex="-1" href="/ko/firefox/aurora/">Firefox Aurora</a></li><li ><a tabindex="-1" href="/ko/firefox/beta/">Firefox Beta</a></li><li ><a tabindex="-1" href="/ko/firefox/fx/#desktop">Firefox</a></li><li  class="last"><a tabindex="-1" href="/ko/firefox/organizations/">Firefox 기관용</a></li></ul></li>
+    <li id="nav-main-addons" ><a aria-haspopup="true" aria-owns="nav-main-addons-submenu" tabindex="0" href="https://addons.mozilla.org/">부가 기능</a><ul id="nav-main-addons-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="https://addons.mozilla.org/ko/firefox/">PC용 부가기능</a></li><li ><a tabindex="-1" href="https://addons.mozilla.org/ko/android/">모바일용 부가기능</a></li><li  class="last"><a tabindex="-1" href="https://addons.mozilla.org/ko/firefox/themes/">테마</a></li></ul></li>
+    <li id="nav-main-support" ><a aria-haspopup="true" aria-owns="nav-main-support-submenu" tabindex="0" href="http://support.mozilla.org/">도움말</a><ul id="nav-main-support-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="http://support.mozilla.org/ko/">PC용 도움말</a></li><li  class="last"><a tabindex="-1" href="http://support.mozilla.org/ko/products/mobile">모바일 도움말</a></li></ul></li>
+    <li id="nav-main-about"  class="last"><a aria-haspopup="true" aria-owns="nav-main-about-submenu" tabindex="0" href="/ko/about/">소개</a><ul id="nav-main-about-submenu" aria-expanded="false"><li  class="first"><a tabindex="-1" href="/community/">블로그</a></li><li ><a tabindex="-1" href="/ko/about/">Firefox 소개</a></li><li ><a tabindex="-1" href="http://www.mozilla.org/join">Mozilla 후원(영문)</a></li><li ><a tabindex="-1" href="/ko/about/participate/">참여하기</a></li></ul></li>'), 
 
   // Download button
   array(
@@ -221,6 +277,77 @@ _gaq.push(['_trackPageview']);
           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
       })();
      </script>"), 
+  array(
+    'source' => "<script>
+var _gaq = _gaq || [];
+var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+_gaq.push(['_setAccount', 'UA-36116321-1']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_setAllowAnchor', true]);
+_gaq.push(['_trackPageview']);
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    var prefix = ('https:' == document.location.protocol ? 'https://ssl' :
+                  'http://www');
+    ga.src = prefix + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+</script>", 
+    'target' => "<script>
+        var _gaq = _gaq || [];
+       _gaq.push(['_setAccount', 'UA-53785-1']);
+       _gaq.push(['_setDomainName', 'mozilla.or.kr']);
+       _gaq.push(['_trackPageview']);
+
+       (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+     </script>"), 
+
+  // Aurora Footer
+  array(
+    'source' => 'Let’s be <span>friends!</span>', 
+    'target' => '소셜웹 <span>공식채널</span>'), 
+  array(
+    'source' => '<a href="http://twitter.com/firefox">Twitter</a>', 
+    'target' => '<a href="http://twitter.com/mozillakr">Twitter</a>'), 
+  array(
+    'source' => '<a href="http://facebook.com/Firefox">Facebook</a>', 
+    'target' => '<a href="http://facebook.com/mozillakr">Facebook</a>'), 
+  array(
+    'source' => '<a href="https://affiliates.mozilla.org/">Firefox Affiliates</a>', 
+    'target' => '<a href="http://forums.mozilla.or.kr/">한국커뮤니티</a>'), 
+  array(
+    'source' => '<label for="flang">Other Languages</label>', 
+    'target' => '<label for="flang">다른 언어</label>'), 
+  array(
+    'source' => '<li><a href="/en-US/firefox/features/">Desktop</a><ul><li><a href="/en-US/firefox/features/">Features</a></li><li><a href="/en-US/firefox/customize/">Customize</a></li><li><a href="/en-US/firefox/performance/">Performance</a></li><li><a href="/en-US/firefox/technology/">Technology</a></li><li><a href="/en-US/firefox/security/">Privacy &amp; Security</a></li></ul></li>
+    <li><a href="/en-US/mobile/">Mobile</a><ul><li><a href="/en-US/mobile/">Download</a></li><li><a href="/en-US/mobile/features/">Features</a></li><li><a href="https://addons.mozilla.org/en-US/mobile/?browse=featured">Customize</a></li><li><a href="/en-US/mobile/faq/">FAQ</a></li></ul></li>
+    <li><a href="/en-US/firefox/channel/">Releases</a><ul><li><a href="/en-US/firefox/channel/">Overview</a></li><li><a href="/en-US/firefox/aurora/">Firefox Aurora</a></li><li><a href="/beta/">Firefox Beta</a></li><li><a href="/en-US/firefox/">Firefox</a></li><li><a href="/en-US/firefox/organizations/">Firefox for Organizations</a></li></ul></li>
+    <li><a href="https://addons.mozilla.org/">Add-ons</a><ul><li><a href="https://addons.mozilla.org/firefox/">Desktop Add-ons</a></li><li><a href="https://addons.mozilla.org/mobile/">Mobile Add-ons</a></li><li><a href="http://www.getpersonas.com/">Personas</a></li></ul></li>
+    <li><a href="http://support.mozilla.org/">Support</a><ul><li><a href="http://support.mozilla.org/en-US/kb/">Desktop Support</a></li><li><a href="http://support.mozilla.org/mobile">Mobile Support</a></li></ul></li>
+    <li><a href="/en-US/firefox/about/">About</a><ul><li><a href="http://blog.mozilla.com/">Blog</a></li><li><a href="/en-US/firefox/about/">About Firefox</a></li><li><a href="http://www.mozilla.org/join">Join Mozilla</a></li><li><a href="/en-US/about/participate/">Participate</a></li><li><a href="/en-US/press/">Press Center</a></li><li><a href="/en-US/firefox/brand/">Brand Toolkit</a></li><li><a href="/en-US/about/careers.html">Careers</a></li><li><a href="/en-US/about/partnerships.html">Partnerships</a></li><li><a href="/en-US/about/contact.html">Contact Us</a></li></ul></li>', 
+    'target' => '<li><a href="/ko/firefox/features/">데스크탑</a><ul><li><a href="/ko/firefox/features/">주요 기능</a></li><li><a href="/ko/firefox/customize/">부가 기능</a></li><li><a href="/ko/firefox/performance/">성능 향상</a></li><li><a href="/ko/firefox/technology/">웹 기술</a></li><li><a href="/ko/firefox/security/">보안 및 정보 보호</a></li></ul></li>
+    <li><a href="/ko/mobile/">모바일</a><ul><li><a href="/ko/mobile/">다운로드</a></li><li><a href="/ko/mobile/features/">주요 기능</a></li><li><a href="https://addons.mozilla.org/ko/mobile/?browse=featured">부가 기능</a></li><li><a href="/ko/mobile/faq/">FAQ &shy;</a></li></ul></li>
+    <li><a href="/ko/firefox/channel/">다운로드</a><ul><li><a href="/ko/firefox/channel/">채널별</a></li><li><a href="/ko/firefox/aurora/">Firefox Aurora</a></li><li><a href="/ko/firefox/beta/">Firefox Beta</a></li><li><a href="/ko/firefox/fx/#desktop">Firefox</a></li><li><a href="/ko/firefox/organizations/">Firefox 기관용</a></li></ul></li>
+    <li><a href="https://addons.mozilla.org/">부가 기능</a><ul><li><a href="https://addons.mozilla.org/ko/firefox/">PC용 부가기능</a></li><li><a href="https://addons.mozilla.org/ko/android/">모바일용 부가기능</a></li><li><a href="https://addons.mozilla.org/ko/firefox/themes/">테마</a></li></ul></li>
+    <li><a href="http://support.mozilla.org/">도움말</a><ul><li><a href="http://support.mozilla.org/ko/">PC용 도움말</a></li><li><a href="http://support.mozilla.org/ko/products/mobile">모바일 도움말</a></li></ul></li>
+    <li><a href="/ko/about/">소개</a><ul><li><a href="http://blog.mozilla.org/">블로그(영문)</a></li><li><a href="/ko/about/">Firefox 소개</a></li><li><a href="http://www.mozilla.org/join">Mozilla 후원(영문)</a></li><li><a href="/ko/about/participate/">참여하기</a></li><li><a href="http://blog.mozilla.org/press/">보도자료</a></li><li><a href="/ko/firefox/brand/">로고 이용(영문)</a></li><li><a href="/ko/about/careers.html">채용문의 (영문)</a></li><li><a href="/ko/about/partnerships.html">제휴 안내</a></li><li><a href="/ko/about/contact.html">연락처</a></li></ul></li>'), 
+  array(
+    'source' => 'Privacy Policy', 
+    'target' => '개인 정보 보호 정책 (영문)'), 
+  array(
+    'source' => 'Legal Notices', 
+    'target' => '법적 고지 (영문)'), 
+  array(
+    'source' => 'Report Trademark Abuse', 
+    'target' => '상표권 침해 신고 (영문)'), 
 );
 
 $replace_rule = array_merge($common_rule, $replace_rule);
