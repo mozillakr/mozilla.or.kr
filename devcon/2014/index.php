d004ac77a5455c190a9cd6b081a0c33b87a56c71
Channy Yun <channy@gmail.com>
2014-03-18 15:28:04 +0900

    Update index.php

--- devcon/2014/index.php
+++ devcon/2014/index.php
@@ -1,1 +1,198 @@
-Initial commit 
+<!DOCTYPE html>
+<head>
+<title>Firefox 개발자콘퍼런스 2014</title>
+<meta charset="utf8">
+</head>
+<body>
+<header>
+   <h1>Firefox 개발자콘퍼런스 2014</h1>
+   <p>함께 개방웹 기술 변화를 느껴보세요!</p>
+</headr>
+
+<nav>
+<!-- https://fbf8.com/ 스타일의 디자인이면 좋을듯 -->
+<ul>
+   <li class="intro">행사소개</li>
+   <li class="program">프로그램</li>
+   <li class="event">이벤트</li>
+   <li class="call">발표신청</li> 
+   <li class="register">등록하기</li>
+</ul>
+</nav>
+
+
+<article id="#welcome">
+
+<h2>Firefox 개발자 콘퍼런스에 초대합니다!</h2>
+<p>Mozilla 한국커뮤니티는 웹 탄생 25주년과 국내 도입 20주년을 맞아 국내 처음으로 Firefox 개발자 콘퍼런스를 개최합니다.</p>
+
+<p>인터넷과 웹 세상의 개방성, 혁신 그리고 기회를 위해 최선을 다해 온 글로벌 비영리 커뮤니티인 Mozilla의 웹 브라우저인 Firefox와 모바일 웹 운영체제인 Firefox OS 그리고 HTML5를 비롯 최신 웹 표준 기술의 변화를 느낄 수 있는 기회입니다.</p>
+
+<p>본 행사는 Mozilla 제품 및 웹 표준 기술에 관심있는 개발자와 디자이너, 고급 사용자를 대상으로 기술 데모와 강연, 정보 교환을 할 수 있습니다. 그리고, 국내외 Mozilla 커뮤니티 활동가와의 만남과 최신 Firefox  OS 단말기를 직접 접할 수 있습니다.  </p>
+
+<p>관심 있는 분들의 많은 참여를 바랍니다!</p>
+</article>
+
+<article id="#intro">
+<h2> 행사 개요</h2>
+
+<ul>
+  <li>일시: 2014년 4 월 10 일(목) 오전 10:00~ 18:20
+  <li>장소: 서울 강남 코엑스 콘퍼런스 센터 401호 (약도보기)
+  <li>참가비: 5,000원 (점심 식사 제공)
+  <li>인원수: 400명 (정원이 되는 대로 마감합니다!)
+  <li>주최: Mozilla 한국 커뮤니티
+  <li>후원: 다음커뮤니케이션, Mozilla Corp.
+  <li>해시태그: #fxdevconkr 
+</ul>
+
+</article>
+<h2>상세 프로그램</h2>
+<p>Mozilla 커뮤니티 및 웹 개발자를 위한 기술 동향, Firefox OS와 구현 사례 등에 대해 알차게 꾸몄습니다.</p>
+
+<article id="#program">
+<ul>
+<li><div class="time"><span>10:00~10:10</span></div>
+    <div class="title"><h3>인사말</h3></div>
+    <div class="desc"><p></p></div>
+    <div class="speaker"><strong>신현석(Mozilla 한국커뮤니티 대표, Daum)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>10:10~10:50</span></div>
+    <div class="title"><h3>Mozilla's Open Web Strategy</h3></div>
+    <div class="desc"><p>For closed Internet and mobile environments, we discuss Mozilla's mission and future plan for web developers.</p></div>
+    <div class="speaker"><strong>Gen Kanai(Mozilla Corp.)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>10:50~11:40</span></div>
+    <div class="title"><h3>Firefox Developer Tools</h3></div>
+    <div class="desc"><p>Developing for any devices is a challenge. We will go through some of Web developement tools for Firefox to make your job easier.</p></div>
+    <div class="speaker"><strong>Karl Dubost(Mozilla Corp.)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>11:40~13:00</span></div>
+    <div class="break"><h3>점심 식사</h3></div>
+</li>
+
+<li><div class="time"><span>13:00~13:30</span></div>
+    <div class="title"><h3>Firefox OS 소개</h3></div>
+    <div class="desc"><p>Firefox OS에 대한 개략적 소개와 구조와 웹앱 생태계에 대해 소개합니다.</p></div>
+    <div class="speaker"><strong>신현석(Mozilla 한국커뮤니티, Daum)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+
+<li><div class="time"><span>13:30~14:00</span></div>
+    <div class="title"><h3>Firefox OS  한국어 키보드 개발 사례</h3></div>
+    <div class="desc"><p>Firefox OS의 오픈 소스 구조 소개 및 한국어 키보드 개발 사례 및 소감을 발표합니다.</p></div>
+    <div class="speaker"><strong>변상필(다음커뮤니케이션)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>14:00~14:30</span></div>
+    <div class="title"><h3>라이트닝 토크</h3></div>
+    <div class="desc"><p>Mozilla 커뮤니티 활동 및 HTML5 웹 앱 기술에 대한 일반 참가자들의 5분 토크가 펼쳐집니다.</p></div>
+    <div class="speaker"><strong>사회: 설수웅(Mozilla 한국커뮤니티, 와이즈넛)</strong></div>
+    <ul>
+       <li>참가 신청은 '발표 신청'을 이용해 주세요.
+       <li>5분내에서 발표 가능해야 합니다.
+       <li>주제는 Mozilla 활동, 웹 표준 기술, 웹 앱 개발 사례 등입니다.
+    </ul>
+</li>
+
+<li><div class="time"><span>14:30~15:00</span></div>
+    <div class="break"><h3>휴식 및 전시 부스 관람</h3></div>
+</li>
+
+<li><div class="time"><span>15:00~15:30</span></div>
+    <div class="title"><h3>HTML5 Web Standardization</h3></div>
+    <div class="desc"><p></p></div>
+    <div class="speaker"><strong>David Daniel (W3C)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>15:30~16:00</span></div>
+    <div class="title"><h3>HTML5 게임 엔진 개발 사례</h3></div>
+    <div class="desc"><p></p></div>
+    <div class="speaker"><strong>김준기 (SK플래닛)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>16:00~16:30</span></div>
+    <div class="title"><h3>미래 웹 기술</h3></div>
+    <div class="desc"><p>서버 없는 WebRTC, Emscripten을 통한 asm.js 기반 게임 엔진 등 Mozilla가 선도하는 웹표준 기술을 소개합니다.</p></div>
+    <div class="speaker"><strong>윤석찬 (Mozilla 한국커뮤니티, Daum)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>16:30~16:40</span></div>
+    <div class="break"><h3>휴식</h3></div>
+</li>
+
+<li><div class="time"><span>16:40~17:00</span></div>
+    <div class="title"><h3>Firefoxweb 단말기 제조 사례</h3></div>
+    <div class="desc"><p>브라질에 시판 중인 Firefox OS 단말기인 Fireweb의 기기 제조 경험을 공유합니다.</p></div>
+    <div class="speaker"><strong> OOO (LG전자)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>17:00~17:40</span></div>
+    <div class="title"><h3>Firefox OS용 라인앱 개발 사례</h3></div>
+    <div class="desc"><p>글로벌 모바일 메신저인 Line앱을 개발을 위한 HTML5 및 Mozilla Web API 사용 경험 및 문제 해결 사례를 공유합니다.</p></div>
+    <div class="speaker"><strong>차기용 (라인플러스)</strong></div>
+    <div class="photo"><!--IMG--></div>
+</li>
+
+<li><div class="time"><span>17:40~18:00</span></div>
+    <div class="break"><h3>폐회 및 행운권 추첨(Firefox OS 단말기)</h3></div>
+</li>
+</ul>
+
+</article>
+
+<article id="#event">
+<h2>이벤트 및 전시 부스</h2>
+
+<h3>공유 이벤트</h3>
+<p>본 행사를 소셜 서비스에 공유해 주세요! 공유하신 분을 추첨하여 Firefox OS 단말기 등 푸짐한 상품을 드립니다.</p>
+
+
+
+<h3>전시 부스</h3>
+<p>본 행사장 앞에는 Firefox OS의 출시 단말기 및 Mozilla 커뮤니티 활동 부스가 열리게 됩니다. 여러분의 많은 참여를 바랍니다.</p>
+
+</article>
+
+<article id="#call">
+<h2>라이트닝 토크 발표 신청</h2>
+<ul>
+    <li>주제는 Mozilla 활동, 웹 표준 기술, 웹 앱 개발 사례 등입니다.
+    <li>5분내에서 발표 가능해야 합니다.
+    <li>프리젠테이션은 HTML5 템플릿을 이용해야 합니다.
+</ul>
+<a href="#googleform"><strong>발표 신청 바로하기<strong></a>
+</article>
+
+<article id="#register">
+<h2>사전 등록하기</h2>
+
+<ul>
+   <li>참가비(5,000원)는 다른 분들의 참여 기회를 높히기 위해 받는 것임을 양해해 주시고, 전액 Mozilla 재단에 기부됩니다.
+   <li>참가자에게는 점심 식사와 Mozilla와 메인 후원사인 Daum이 준비한 각종 기념물품을 선물로 드립니다.
+   <li>참가 정원은 400명이며, 결제자 선착순으로 마감됩니다.
+</ul>
+<a href="#onoffmix"><strong>참가 신청 바로하기</strong></a>
+</article>
+
+<footer>
+<ul>
+  <li>오시는 길
+  <li>FAQ
+</ul>
+<p>Copyright(c), Mozilla 한국커뮤니티. Creative Commons</p>
+</footer>
+</body>
+</html>
