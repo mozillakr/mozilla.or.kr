dfa9c768f787a2b9478194ffd1e8a4e5f56dc22d
Hyeonseok Shin <hyeonseok@gmail.com>
2016-09-13 20:50:37 +0900

    Add static tabzilla.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync -rltDvz --delete www/index.html www/.htaccess ec2-user@ec2-54-64-61-67.ap-northeast-1.compute.amazonaws.com:/data/mozilla.or.kr/
+rsync -rltDvz --delete www/index.html www/.htaccess www/tabzilla ec2-user@ec2-54-64-61-67.ap-northeast-1.compute.amazonaws.com:/data/mozilla.or.kr/
